3d5b49ab06856ceba991e40d8ef99dcdd640f6fb
twarbelow <taija.warbelow@google.com>
2020-07-17 09:53:04 -0700

    show spec

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'show page' do
+  it 'shows name, experience, names of rides they work on, usable form to add to workload' do
+    m = Mechanic.create(name: "Penny", experience: 6)
+    r1 = m.rides.create(name: "Big Bad Betty", thrill_rating: 8)
+    r2 = m.rides.create(name: "Loop-d-Swoop-d-Loop", thrill_rating: 5)
+    r3 = Ride.create(name: "Little Woah", thrill_rating: 10)
+
+    visit "/mechanics/#{m.id}"
+
+    expect(page).to have_content("Mechanic: #{m.name}")
+    expect(page).to have_content("Years of experience: #{m.experience}")
+    expect(page).to have_content("#{r1.name}")
+    expect(page).to have_content("#{r2.name}")
+    expect(page).to have_content("Add a ride to workload:")
+    fill_in :ride, with: "#{r3.id}"
+    click_button "Submit"
+    expect(path).to eq "/mechanics/#{m.id}"
+    expect(page).to have_content("#{m3.name}")
+  end
+end
+
+# Story 3
+# As a user,
+# When I go to a mechanics show page
+# I see their name, years of experience, and names of all rides they’re working on
+# And I also see a form to add a ride to their workload
+# When I fill in that field with an id of an existing ride and hit submit
+# I’m taken back to that mechanics show page
+# And I see the name of that newly added ride on this mechanics show page
+# Ex:
+# Mechanic: Kara Smith
+# Years of Experience: 11
+# Current rides they’re working on:
+# The Frog Hopper
+# Fahrenheit
+# The Kiss Raise
+# Add a ride to workload:
+# _pretent_this_is_a_textfield_
+#                       Submit
