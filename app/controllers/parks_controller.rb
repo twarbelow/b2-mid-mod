33f51aaea1889882cc9d4ccf95e154d06a6bdf45
twarbelow <taija.warbelow@google.com>
2020-07-17 09:36:42 -0700

    controller, view, route, for park show spec. test passing, would like to figure out how to get decimals to 2 places

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,5 +1,7 @@
 class ParksController < ApplicationController
   def show
     @park = Park.find(params[:id])
+    @rides = @park.rides.order(:name)
+    @average = @rides.average(:thrill_rating)
   end
 end
--- spec/features/parks/show_spec.rb
+++ spec/features/parks/show_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe 'show page' do
   it "shows name and price of park, names of rides at park, average thrill rating of rides at park" do
     park = Park.create(name: "Roller Coaster Realm", price: 50.00)
-    r1 = Ride.create(name: "Big Bad Betty", thrill_rating: 8)
-    r2 = Ride.create(name: "Loop-d-Swoop-d-Loop", thrill_rating: 5)
-    r3 = Ride.create(name: "Little Woah", thrill_rating: 10)
+    r1 = park.rides.create(name: "Big Bad Betty", thrill_rating: 8)
+    r2 = park.rides.create(name: "Loop-d-Swoop-d-Loop", thrill_rating: 5)
+    r3 = park.rides.create(name: "Little Woah", thrill_rating: 10)
 
     visit "parks/#{park.id}"
-
+    save_and_open_page
     expect(page).to have_content("#{park.name}")
     expect(page).to have_content("Admission: $#{park.price}")
 
-    expect(page).to have_content("1. #{r1.name}")
-    expect(page).to have_content("2. #{r3.name}")
-    expect(page).to have_content("3. #{r2.name}")
-  expect(page).to have_content("Average Thrill Rating of Rides: 7.6/10")
+    expect(page).to have_content("#{r1.name}")
+    expect(page).to have_content("#{r3.name}")
+    expect(page).to have_content("#{r2.name}")
+    expect(page).to have_content("Average Thrill Rating of Rides: 7.7/10")
   end
 end
 
