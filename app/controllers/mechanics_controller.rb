0af477b1376e30df1d9760a9dfcd62a3306f3933
twarbelow <taija.warbelow@google.com>
2020-07-17 10:29:34 -0700

    add update action

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,11 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def update
+    mechanic = Mechanic.find(params[:id])
+    new_ride = Ride.find(params[:ride_id])
+    mechanic.rides << new_ride
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
